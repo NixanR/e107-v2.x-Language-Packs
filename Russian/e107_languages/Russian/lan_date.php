083088301a8813258efa10ae4d09a6e27ce832d2
Ruchinskas Vladimir <nixanr@gmail.com>
2017-01-09 17:19:43 +0600

    Another, the translation adjustment.

--- Russian/e107_languages/Russian/lan_date.php
+++ Russian/e107_languages/Russian/lan_date.php
@@ -16,7 +16,7 @@ define("LANDT_03", "неделя");
 define("LANDT_04", "день");
 define("LANDT_05", "час");
 define("LANDT_06", "минута");
-define("LANDT_07", "второй");
+define("LANDT_07", "секунда");
 define("LANDT_01s", "лет");
 define("LANDT_02s", "месяца");
 define("LANDT_03s", "недели");
@@ -24,11 +24,13 @@ define("LANDT_04s", "дней");
 define("LANDT_05s", "часа");
 define("LANDT_06s", "минут");
 define("LANDT_07s", "секунд");
-define("LANDT_08", "мин.");
+
+define("LANDT_08", "мин");
 define("LANDT_08s", "мин.");
-define("LANDT_09", "сек.");
+define("LANDT_09", "сек");
 define("LANDT_09s", "сек.");
 define("LANDT_AGO", "назад");
+
 define("LANDT_10", "Сейчас");
 
 
