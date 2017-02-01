b31897dbfc453266e835b5998cc97c5def7a856a
Ruchinskas Vladimir <nixanr@gmail.com>
2017-02-01 19:29:09 +0600

    new file

---
+++ Russian/e107_languages/Russian/lan_form_handler.php
@@ -0,0 +1,31 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Russian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/01/21 16:03:28
+|
+|        $Author: Admin $
++---------------------------------------------------------------+
+*/
+
+define("LAN_EFORM_001", "Нажмите на аватар, чтобы изменить его");
+define("LAN_EFORM_002", "Выберите аватар");
+define("LAN_EFORM_003", "или");
+define("LAN_EFORM_004", "Выбрать этот аватар");
+define("LAN_EFORM_005", "Нет  доступных аватаров");
+define("LAN_EFORM_006", "Уведомление для администратора: [br]Папка [b][x][/b] пустая.[br]Загрузите в папку, какие-нибудь изображения, для использования аватаров по умолчанию.");
+define("LAN_EFORM_007", "Управление медиа");
+define("LAN_EFORM_008", "Выбрать столбцы для отображения");
+define("LAN_EFORM_009", "Показать столбцы");
+define("LAN_EFORM_010", "Быстрый просмотр");
+define("LAN_EFORM_011", "Перейти к  профилю пользователя");
+define("LAN_EFORM_012", "Многоязыковое поле");
+define("LAN_EFORM_013", "перейти к списку");
+define("LAN_EFORM_014", "создать другой");
+define("LAN_EFORM_015", "редактировать");
+define("LAN_EFORM_016", "После отправки:");
+
+
+?>
